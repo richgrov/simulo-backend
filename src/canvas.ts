593fb6288eee8b8d7a827db4225dc10d90dae2d9
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-17 15:19:53 -0600

    Remove unused import

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-import { supabase } from "./auth/supabase";
 import { Machine } from "./models";
 import { showCallout } from "./callout";
 
